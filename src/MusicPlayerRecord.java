648fed75898f407c96f3cb9f273cb8c68226f844
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-03-19 16:57:19 -0300

    Correcao

--- src/MusicPlayerRecord.java
+++ src/MusicPlayerRecord.java
@@ -2,16 +2,16 @@ public record MusicPlayerRecord(String filename) implements MusicPlayer {
 
     @Override
     public void reproduzir() {
-        System.out.println("Reproduzindo musica" + this.filename);
+        System.out.println("Reproduzindo musica " + this.filename);
     }
 
     @Override
     public void pausar() {
-        System.out.println("Pausando a musica" + this.filename);
+        System.out.println("Pausando a musica " + this.filename);
     }
 
     @Override
     public void parar() {
-        System.out.println("Parando a musica" + this.filename);
+        System.out.println("Parando a musica " + this.filename);
     }
 }
